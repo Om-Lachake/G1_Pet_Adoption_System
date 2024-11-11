cbcea7ae60588503a8ad96e69bc009897623e543
Om-Lachake <omklachake@gmail.com>
2024-11-11 23:14:44 +0530

    Delete routes directory

--- routes/mainRoutes.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const {logout}=require("../controllers/loginControllers")
-
-//routing
-router.get("/logout",logout)
-
-//export module
-module.exports=router
