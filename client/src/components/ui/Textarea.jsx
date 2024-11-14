f833ff0258772b478656ccfe569b6a8524122ed1
Om-Lachake <omklachake@gmail.com>
2024-11-14 22:25:23 +0530

    Update Textarea.jsx
    Making textarea customizable

--- client/src/components/ui/Textarea.jsx
+++ client/src/components/ui/Textarea.jsx
@@ -1,12 +1,12 @@
-const TextArea = ({ icon: Icon, ...props }) => {
+const TextArea = ({ icon: Icon,iconKeep='true',textColor='text-white',borderColor='border-blue-400',bgColor = 'bg-blue-600', placeHolderColor='placeholder-zinc-100',...props }) => {
 	return (
 		<div className='relative mb-6'>
-			<div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-				<Icon className='size-5 text-green-500' />
-			</div>
+			{iconKeep === 'true' && <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+		  <Icon className={`size-5 text-gray-500`} />
+		</div>}
 			<textarea
 				{...props}
-				className='w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-500 text-white placeholder-gray-400 transition duration-200 resize-none'
+				className={`w-full ${iconKeep === "true" ? "pl-10" : "pl-4"} pr-3 py-2 ${bgColor} opacity-85 rounded-lg border ${borderColor}  ${textColor}  ${placeHolderColor} transition duration-200 resize-none`}
 			/>
 		</div>
 	);
