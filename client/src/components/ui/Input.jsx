a62d53ba2f58681212e1557acf7f0b5a57004af1
Om-Lachake <omklachake@gmail.com>
2024-11-14 22:24:11 +0530

    Update Input.jsx
    Input element can be customized according to the user

--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -1,14 +1,15 @@
-const Input = ({ icon: Icon, ...props }) => {
+const Input = ({ icon: Icon,iconKeep='true',textColor='text-white',borderColor='border-blue-700' ,bgColor = 'bg-blue-500',placeHolderColor='placeholder-zinc-100', ...props }) => {
 	return (
-		<div className='relative mb-6'>
-			<div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
-				<Icon className='size-5 text-gray-500' />
-			</div>
-			<input
-				{...props}
-				className='w-full pl-10 pr-3 py-2 bg-gradient-to-r from-blue-600 to-blue-500 opacity-85 rounded-lg border border-blue-700  text-white placeholder-zinc-100 transition duration-200' 
-			/>
-		</div>
+	  <div className='relative mb-6'>
+		{iconKeep === 'true' && <div className='absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none'>
+		  <Icon className={`size-5 text-gray-500`} />
+		</div>}
+		<input
+		  {...props}
+		  className={`w-full ${iconKeep === "true" ? "pl-10" : "pl-4"} pr-3 py-2 ${bgColor} opacity-85 rounded-lg border ${borderColor} ${textColor}  ${placeHolderColor} transition duration-200`}
+		/>
+	  </div>
 	);
-};
-export default Input;
+  };
+  export default Input;
+  
