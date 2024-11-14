9632f53a37076489a619f5a9230a422ef0bc4435
Om-Lachake <omklachake@gmail.com>
2024-11-14 22:43:25 +0530

    Update layout.jsx
    Passing useState to control toggling of side bar

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import AdminSideBar from './sidebar'
 import AdminHeader from './header'
 
 const AdminLayout = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <div className='flex min-h-screen w-full'>
         {/* admin sidebar */}
-        <AdminSideBar/>
+        <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
         <div className="flex flex-1 flex-col">
             {/* Admin Header */}
-            <AdminHeader/>
+            <AdminHeader setOpen={setOpenSidebar} />
             <main className='flex-1 flex bg-muted/40 p-4 md:p-6'>
                 <Outlet/>
             </main>
