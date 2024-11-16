27d058af0d1f9678946459bd956d6a4eb9b201a9
anjalichandwani12 <202201032@daiict.ac.in>
2024-11-16 19:09:59 +0530

    Change in layout

--- client/src/components/shopping-view/layout.jsx
+++ client/src/components/shopping-view/layout.jsx
@@ -6,7 +6,7 @@ const ShoppingLayout = () => {
     return (
         <div className="flex flex-col bg-white overflow-hidden">
           {/* common header */}
-          <ShoppingHeader/>
+  
           <main className="flex flex-col w-full">
             <Outlet />
           </main>
