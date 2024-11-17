4e7de40d9ce38b5a415f16b190e3b9a3369bd786
Om-Lachake <omklachake@gmail.com>
2024-11-17 11:41:40 +0530

    Create firebaseConfig.js
    Configurating firebase for uploading image

---
+++ server/service/firebaseConfig.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'gs://happytails-902d9.appspot.com',
+});
+
+const bucket = admin.storage().bucket();
+
+module.exports = { bucket };
