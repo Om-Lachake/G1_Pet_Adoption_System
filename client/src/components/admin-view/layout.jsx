ec9f67a1f3e01a3ff4080f6eb9de238e4bd441db
Om-Lachake <omklachake@gmail.com>
2024-11-17 18:40:02 +0530

    Update layout.jsx
    Adding background image in admin panel

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,24 +1,30 @@
-import React, { useState } from 'react'
-import { Outlet } from 'react-router-dom'
-import AdminSideBar from './sidebar'
-import AdminHeader from './header'
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import AdminSideBar from "./sidebar";
+import AdminHeader from "./header";
 
 const AdminLayout = () => {
   const [openSidebar, setOpenSidebar] = useState(false);
 
   return (
-    <div className='flex min-h-screen w-full'>
-        {/* admin sidebar */}
-        <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
-        <div className="flex flex-1 flex-col">
-            {/* Admin Header */}
-            <AdminHeader setOpen={setOpenSidebar} />
-            <main className='flex-1 flex flex-col bg-muted/40 p-4 md:p-6'>
-                <Outlet/>
-            </main>
-        </div>
-    </div>
-  )
-}
+    <div className="flex min-h-screen w-full ">
+      {/* admin sidebar */}
+      <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
+      <div className="flex flex-1 flex-col">
+        {/* Admin Header */}
+        <AdminHeader setOpen={setOpenSidebar} />
+        <main className="relative flex-1 flex flex-col p-4 md:p-6 bg-auto bg-center bg-fixed bg-[url('/images/myImage.jpeg')]">
+          {/* Overlay */}
+          <div className="absolute inset-0 bg-zinc-100 bg-opacity-70"></div>
 
-export default AdminLayout
+          {/* Content */}
+          <div className="relative z-10">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default AdminLayout;
